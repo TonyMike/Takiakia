ae3ecce0827cea6b4616d852c1665d939b88e593
TonyMike <tee.jhay1@gmail.com>
2022-12-17 13:17:18 +0100

    Added vercel analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,7 @@
 import '../styles/globals.css'
 import { AppProps } from 'next/app'
 import { createTheme, ThemeProvider } from '@mui/material'
+import { Analytics } from '@vercel/analytics/react'
 
 const theme = createTheme({
   breakpoints: {
@@ -18,6 +19,7 @@ export default function App ({ Component, pageProps }: AppProps) {
   return (
     <ThemeProvider theme={theme}>
       <Component {...pageProps} />
+      <Analytics />
     </ThemeProvider>
   )
 }

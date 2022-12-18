2bc3a27b92a4b499886c4d424baf4bba028736d0
TonyMike <tee.jhay1@gmail.com>
2022-12-18 06:22:50 +0100

    added category icons

--- Components/Category/Category.tsx
+++ Components/Category/Category.tsx
@@ -4,24 +4,24 @@ import { TfiMenuAlt } from 'react-icons/tfi'
 
 const Category = () => {
   const categories = [
-    { name: 'Phones & Tablets', icon: 'text' },
-    { name: 'Electronics', icon: 'text' },
-    { name: 'computer', icon: 'text' },
-    { name: 'Electronics', icon: 'text' },
-    { name: 'fashion', icon: 'text' },
-    { name: 'Sport', icon: 'text' },
-    { name: 'Beauty', icon: 'text' },
-    { name: 'Home & Appliances', icon: 'text' },
-    { name: 'Furnitures', icon: 'text' },
-    { name: 'vehichles', icon: 'text' },
-    { name: 'Property', icon: 'text' },
-    { name: 'Musical Instrument', icon: 'text' }
+    { name: 'Phones & Tablets', icon: '/icons/smartphone.png' },
+    { name: 'Electronics', icon: '/icons/electronics.png' },
+    { name: 'computer', icon: '/icons/computer.png' },
+    { name: 'fashion', icon: '/icons/fashion.png' },
+    { name: 'Sport', icon: '/icons/sport.png' },
+    { name: 'Game', icon: '/icons/game.png' },
+    { name: 'Beauty', icon: '/icons/beauty.png' },
+    { name: 'Home & Appliances', icon: '/icons/home-appliance.png' },
+    { name: 'Furnitures', icon: '/icons/furnitures.png' },
+    { name: 'vehichles', icon: '/icons/vehicles.png' },
+    { name: 'Property', icon: '/icons/house.png' },
+    { name: 'Musical Instrument', icon: '/icons/musical-instrument.png' }
   ]
   return (
     <div className=' bg-white h-[100%] md:shadow-md '>
       {/* mobile category */}
       <div className=' md:hidden mb-4'>
-        <Grid container rowGap={1.2} columnGap={1.2}>
+        <Grid container rowGap={0.7} columnGap={0.7}>
           {categories.map((category, index) => {
             return (
               <Grid
@@ -29,31 +29,35 @@ const Category = () => {
                 item
                 xs={3.7}
                 sm={2.7}
-                container
+                // container
+                // direction='column'
                 justifyContent='center'
-                className='bg-[#F7FAFE] px-2 py-4 sm:px-4 flex flex-col sm:flex-col space-y-2 justify-between items-center'
+                // alignItems='center'
+                className='bg-[#F7FAFE] py-3 px-2'
               >
-                <Grid item className='h-[50px] w-[50px] rounded-full relative'>
+                <div className='flex flex-col items-center space-y-2 '>
                   <Image
-                    src='/images/iphone13.jpg'
-                    layout='fill'
+                    src={category.icon}
+                    width={40}
+                    height={20}
+                    // layout='fill'
+                    className='mb-2'
                     alt='icon'
-                    className=' rounded-full h-full w-full'
                   />
-                </Grid>
-                <Grid>
-                  <p className='text-[12px] sm:text-[13px] text-navyBlue font-semibold capitalize text-center'>
+
+                  <p className='text-[12px] sm:text-[13px]  text-navyBlue font-semibold text-center capitalize'>
                     {category.name}
                   </p>
-                </Grid>
+                </div>
               </Grid>
             )
           })}
         </Grid>
       </div>
+
       {/* desktop categories */}
       <div className='hidden md:block'>
-        <h3 className='flex pl-5 pr-20  py-5 md:pl-10 items-center space-x-2'>
+        <h3 className='flex pr-20  py-5 md:pl-5 lg:pl-6 items-center space-x-2'>
           <span className='mb-1'>
             <TfiMenuAlt />
           </span>
@@ -66,9 +70,10 @@ const Category = () => {
             return (
               <li
                 key={index}
-                className='capitalize pl-5 p-20 pr-1 py-2 md:pl-10 text-[15px] text-[#3d3d3d] hover:bg-pink hover:text-white transition-all ease-in-out duration-200 cursor-pointer hover:ease-in '
+                className='capitalize  p-20 pr-1 py-2 md:pl-5 lg:pl-6 text-[15px] flex space-x-2 text-[#3d3d3d] hover:bg-pink hover:text-white transition-all ease-in-out duration-200 cursor-pointer hover:ease-in '
               >
-                {category.name}
+                <Image src={category.icon} width={20} height={20} alt='icon' />
+                <span>{category.name}</span>
               </li>
             )
           })}

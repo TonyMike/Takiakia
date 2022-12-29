6f757026d4abfe60bd5367af5066bb0626015bf0
TonyMike <tee.jhay1@gmail.com>
2022-12-29 16:44:55 +0100

    product title

--- Components/ProductDetails/ProductDetails.tsx
+++ Components/ProductDetails/ProductDetails.tsx
@@ -3,21 +3,32 @@ import DetailsOne from './DetailsOne'
 import ImageDisplay from './ImageDisplay'
 import ProductDescription from './ProductDescription'
 
+const ProductTitle = ({ title }: { title: string }) => {
+  return (
+    <div>
+      <h1 className='text-2xl capitalize '>{title}</h1>
+      {/* <h1 className='text-2xl capitalize '>{title}</h1> */}
+    </div>
+  )
+}
 const ProductDetails = () => {
   return (
-    <div className='m:p-10'>
+    <div className='md:p-10'>
       <Grid container>
         <Grid item xs={12} md={8} bgcolor='red' className=''>
           <ImageDisplay />
         </Grid>
-        <Grid item xs={12} md={0} className='md:hidden'>
-          <h1 className='text-2xl capitalize'>Iphone 13 Pro Max</h1>
+        <Grid item xs={12} md={0} className='md:hidden px-3'>
+          <ProductTitle title='Iphone 13 Pro Max' />
         </Grid>
         <Grid item xs={12} md={4}>
           <DetailsOne />
         </Grid>
       </Grid>
-      <h1 className='hidden md:block'>Iphone 13 Pro Max</h1>
+      <div className='px-3 hidden md:block'>
+        <ProductTitle title='Iphone 13 Pro Max' />
+      </div>
+
       <Divider
         className='bg-pink'
         sx={{

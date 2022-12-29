f9b2a2ac8acb347f0b47802230c5ed6da071d23c
TonyMike <tee.jhay1@gmail.com>
2022-12-29 09:07:33 +0100

    details one in progress

--- Components/ProductDetails/DetailsOne.tsx
+++ Components/ProductDetails/DetailsOne.tsx
@@ -1,36 +1,35 @@
 import { NairaIcon } from '../../utils/staticData'
 
-const TimeStamp = () => {
-  return (
-    <div className=' flex items-center text-grey text-[13px]'>
-      <p>Posted 21 hours ago</p>
-      <p>Oyo Ibadan</p>
-    </div>
-  )
-}
 const DetailsOne = () => {
   return (
     <div className='p-3'>
-      <div className='flex items-center justify-between'>
-        <h2 className='text-3xl'>
-          <NairaIcon />
-          {' 280,000'}
-        </h2>
-        <div>
-          <TimeStamp />
+      {/* product price */}
+      <h2 className='text-3xl'>
+        <NairaIcon />
+        {' 280,000'}
+      </h2>
+
+      {/* Image and contact information */}
+      <div className='flex items-center my-3  space-x-3'>
+        <div className='h-16 w-16 rounded-full bg-white shadow-lg'>
+          {/* seller avatarx image */}
+        </div>
+
+        <div className='flex flex-col space-y-1'>
+          <p className='text-pink text-[16px]'>Anthony Oluwajuwon</p>
+          <div className=' flex items-center space-x-2 flex-wrap text-grey text-[12px]'>
+            <p>Posted 21 hours ago</p>
+            <p>Oyo Ibadan</p>
+          </div>
         </div>
       </div>
 
-      <div>
-        <div>{/* seller avatarx image */}</div>
-        <div>
-          <p>Anthony Oluwajuwon</p>
-          <button>Contact Seller</button>
-        </div>
-      </div>
-      <div>
-        <TimeStamp />
-        <p>Oyo Ibadan</p>
+      {/* posted time and location */}
+      <div className='pl-5 mt-3 flex items-center space-x-3 '>
+        <button className='bg-pink rounded-md text-[12px] text-white px-3 py-2 '>
+          Show Number
+        </button>
+        <div>Wa Fb Tw</div>
       </div>
     </div>
   )
--- Components/ProductDetails/ProductDetails.tsx
+++ Components/ProductDetails/ProductDetails.tsx
@@ -9,10 +9,14 @@ const ProductDetails = () => {
         <Grid item xs={12} md={8} bgcolor='red' className=''>
           <ImageDisplay />
         </Grid>
+        <Grid item xs={12} md={0} className='md:hidden' bgcolor='yellow'>
+          <h1>some product name</h1>
+        </Grid>
         <Grid item xs={12} md={4}>
           <DetailsOne />
         </Grid>
       </Grid>
+      <h1>some product name</h1>
     </div>
   )
 }

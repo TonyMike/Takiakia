0b7c52677e92026a8e8a1009455c3830afbfc070
TonyMike <tee.jhay1@gmail.com>
2023-01-24 11:30:23 -0500

    personal details

--- Components/Settings/PersonalDetails.tsx
+++ Components/Settings/PersonalDetails.tsx
@@ -1,9 +1,38 @@
+import InputText from '../../Reusables/InputText'
+
 const PersonalDetails = () => {
   return (
     <div>
-      personal details
+      <form className='flex space-y-4  flex-col'>
+        {/* avatar */}
+        <div>
+          {/* image preview */}
+          <div className='h-20 w-20 bg-grey rounded-full'></div>
+        </div>
+        <div className='flex space-x-4'>
+          <InputText placeholder='First Name' />
+          <InputText placeholder='Last Name' />
+        </div>
+        <InputText type='email' placeholder='Email Address' />
+        <div className='flex space-x-4'>
+          <InputText placeholder='WhatsApp i.e https://wa.me/+2348129384732' />
+          <InputText placeholder='Twitter i.e https://twitter.com/johnsmith' />
+        </div>
+        <InputText
+          type='email'
+          placeholder='Instagram i.e https://instagram.com/johnsmith'
+        />
+        <InputText type='email' placeholder='Phone number i.e 09019404392' />
+
+        <button
+          type='button'
+          className='bg-orange py-2 uppercase font-semibold text-white mt-3'
+        >
+          Submit
+        </button>
+      </form>
     </div>
-  );
+  )
 }
 
-export default PersonalDetails;
+export default PersonalDetails
--- pages/dashboard/settings.tsx
+++ pages/dashboard/settings.tsx
@@ -8,46 +8,36 @@ import DashboardHero from '../../Reusables/DashboardHero'
 
 const Settings: NextPage = () => {
   const [active, setActive] = useState(0)
-  console.log(active)
-  const display = [<PersonalDetails />, <ChangeNumber />, <ChangePassword />]
+  const display = [<PersonalDetails />, <ChangePassword />]
   return (
     <div>
       <Layout>
         {/* <DashboardHero /> */}
         <div className='px-2 py-4'>
-          <ul className='flex text-center my-2'>
+          <ul className='flex text-center my-2 '>
             <li
               onClick={() => setActive(prev => (prev = 0))}
               className={` ${
                 active === 0
                   ? 'bg-orange text-white transition-all duration-100'
                   : null
-              } font-semibold p-2 px-1 capitalize `}
+              } font-semibold p-2 w-1/2 px-1 capitalize `}
             >
               Personal Details
             </li>
+
             <li
               onClick={() => setActive(prev => (prev = 1))}
               className={` ${
                 active === 1
                   ? 'bg-orange text-white transition-all duration-100'
                   : null
-              } font-semibold p-2 px-1 capitalize `}
-            >
-              Phone Number
-            </li>
-            <li
-              onClick={() => setActive(prev => (prev = 2))}
-              className={` ${
-                active === 2
-                  ? 'bg-orange text-white transition-all duration-100'
-                  : null
-              } font-semibold p-2 px-1 capitalize `}
+              } font-semibold p-2 w-1/2 px-1 capitalize `}
             >
               Change password
             </li>
           </ul>
-          {display[active]}
+          <div className='mt-5'>{display[active]}</div>
         </div>
       </Layout>
     </div>
